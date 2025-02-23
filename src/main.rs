32ed3e6b2f976fa95d4af5aaacec03ab4ff31a51
Spero KOTIN <kotin.spero98@gmail.com>
2025-02-23 16:55:42 +0100

    Add newline capability
    Signed-off-by: Spero KOTIN <kotin.spero98@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_something();
 
+    #[warn(clippy::empty_loop)]
     loop {}
-
 }
 
 // Gestionnaire de panique
