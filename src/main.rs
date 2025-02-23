852309115aacf20fb09bacafd68afa21ad635bb3
Spero KOTIN <kotin.spero98@gmail.com>
2025-02-23 16:17:35 +0100

    Impl Writer for VGA mode
    Signed-off-by: Spero KOTIN <kotin.spero98@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
 #![no_std]  // On n'utilise pas la bibliothèque standard
 #![no_main] // On n'utilise pas le point d'entrée standard
 
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
-// Point d'entrée du kernel
-static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
+
 }
 
 // Gestionnaire de panique
