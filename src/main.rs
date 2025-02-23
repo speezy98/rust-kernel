83794c905f575729c8e106124df3ecc80c754f8d
Spero KOTIN <kotin.spero98@gmail.com>
2025-02-23 17:39:31 +0100

    Impl panic message
    Signed-off-by: Spero KOTIN <kotin.spero98@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,15 @@ use core::panic::PanicInfo;
 
 // Gestionnaire de panique
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
