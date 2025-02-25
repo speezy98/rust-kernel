8a1b9f5dc909b31db8433f4d31e9115f1068a912
Spero KOTIN <kotin.spero98@gmail.com>
2025-02-25 22:35:13 +0100

    Mess up

---
+++ tests/basic_boot.rs
@@ -0,0 +1,37 @@
+#![no_std]  // On n'utilise pas la bibliothèque standard
+#![no_main] // On n'utilise pas le point d'entrée standard
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_kernel::println;
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+    
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_kernel::test_panic_handler(info);
+    loop {}
+}
+
+
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
